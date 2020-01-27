89390cd2c4686ee3d9deb673e6ff0ffb35449680
tannavee <tannavee@gmail.com>
2020-01-27 13:05:28 -0800

    sample data structure programs

---
+++ Data Structures/Recursion_Functions.py
@@ -0,0 +1,160 @@
+# Tannavee Kumar
+
+# Problem 1: Creating Kleinfeldt function using recursion
+
+def kleinfeldt(n): # defining function 
+    if n == 1: # if n = 1
+        return 1 
+    else: # else statement if > 1
+        return 1/(n * n) + kleinfeldt(n - 1) # using recursion
+
+
+
+# Problem 2
+
+def ladder(n): # defining function
+    if n == 1: # if the ladder has only 1 rung, then there is only 1 way to go up
+        return 1 
+    elif n == 2: # if the ladder has 2 rungs, there are 2 ways to go up 1 + 1, 2 + 0
+        return 2 
+    else: # if the ladder has > 2 rungs, then use the fibonacci seq
+        return ladder(n - 1) + ladder(n - 2) # using recursion 
+        
+
+
+# Problem 3: Finding largest integer in a list of interest
+
+def findLargest(listOfInterest): # defining function 
+    if len(listOfInterest) == 1: # if the inputed list is only 1 elmt long 
+        return listOfInterest[0] # returns 1st elmt
+    else: # if > 1 elmt 
+        # the reduction step is shortening the first list and making a new list i,
+        # the recursive step is using findLargest simultaneously
+        i = findLargest(listOfInterest[1:]) 
+        if i > listOfInterest[0]: # if given elmt is > 1st elmt
+            return i # return that given elmt
+        else: # if not greater
+            return listOfInterest[0] # return the first element
+
+
+# Problem 4
+
+def findPossibilities(listOfInterest): # defining function, input list "a"
+    listOfPossibilities = [] # list "b"
+    if len(listOfInterest) == 0: # checks if inpted list is empty
+        return listOfPossibilities # returns empty list
+    else:
+        listLen = 0 # defining new variable 
+        if len(listOfInterest) % 2 != 0: # if list is odd
+            listLen = len(listOfInterest) - 1 # length of list goes to 2nd to last elmt
+            listOfPossibilities.append(listOfInterest[-1]) # adds the last elmt to "b"
+        else: # if list is even
+            listLen = len(listOfInterest) # length of list goes to the end
+        i = 0 
+        for i in range(0, listLen, 2): # starts at beginning, goes to the end defined above, and travels 2 elmts at a time
+            if listOfInterest[i] == listOfInterest[i + 1]: # is elmt = the next one
+                listOfPossibilities.append(listOfInterest[i]) # add to b
+        
+    if len(listOfPossibilities) > 2:  # if b > 2
+        return findPossibilities(listOfPossibilities) # do recursion 
+    elif (len(listOfPossibilities) == 2) and (listOfPossibilities[0] == listOfPossibilities[1]): # b has two elements and they're equal to each other
+        listOfPossibilities.pop() # pop off one
+        return listOfPossibilities
+    else:
+        return listOfPossibilities # otherwise just return b
+
+def verifyPossibilities(listOfInterest, listOfPossibilities): # defining function 
+    if len(listOfPossibilities) == 0: # if b is empty 
+        print("There is no majority element since the list of interest is empty.") # lets user know that a is empty
+        return None # returns none
+    elif len(listOfPossibilities) == 1: # is b has 1 elmt
+        i = 0 # counter for list
+        counter = 0 # counter for verifying maj
+        for i in range(0, len(listOfInterest), 1):
+            if listOfInterest[i] == listOfPossibilities[0]: # if elmt in b matches elements in a
+                counter = counter + 1 # counter increases
+        if counter > (len(listOfInterest)) / 2: # if counter > n/2
+            print("The majority element is", listOfPossibilities[0]) # print the element in b
+        else: 
+            print("There is no majority element in: ", listOfInterest) # if not, return none
+            return None
+    else: # if two elmts in b
+        i = 0 # counter to iterate thru a, for 1st elmt in b
+        counter1 = 0 # counter to check for maj, 1st elmt
+        j = 0 # counter to iterate thru a, for 2nd elmt in b
+        counter2 = 0 # counter to check for maj, 2nd elmt
+        for i in range(0, len(listOfInterest), 1): # loop to check if 1st elmt in b is maj
+            if listOfInterest[i] == listOfPossibilities[0]:
+                counter1 = counter1 + 1
+        for j in range(0, len(listOfInterest), 1): # loop to check if 2nd elmt in b is maj
+            if listOfInterest[j] == listOfPossibilities[1]:
+                counter2 = counter2 + 1
+        if counter1 > (len(listOfInterest)) / 2: # if 1st in b is maj
+            print("The majority element is", listOfPossibilities[0]) # let user know 
+        elif counter2 > (len(listOfInterest)) / 2: # if 2nd in b is maj
+            print("The majority element is", listOfPossibilities[1]) # let user know
+        else: # if now maj
+            print("There is no majority element in: ", listOfInterest) # let user know 
+            return None # return none
+
+# Sample Test Data  
+# odD = [1,2,3,3,4,2,1,4,2,1,5,5,5]
+# eveN = [1,2,3,3,5,5]
+# nMajority = [1,2,3]
+# empty = []
+# aOdd = [4,4,3,3,4,2,1,4,2,1,4,5,6,4,12,1,2,31,4,4,4,4,4,3,1,2,3,4,4]
+# liste = [1,2,2,3,4,4]
+# listo = [1,2,1]
+# maj = [4,4,4,4,4,4]
+
+# Using the following Node class code for Problem 5 and 6
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+
+
+# Problem 5: finding a specific item in a linked list via recursion 
+
+def findValue(item, nodeOfInterest): # defining function
+    # if data associated w/ inputed node is not = to item, and there is no node after inputed
+    # node
+    if (nodeOfInterest.getData() != item) and (nodeOfInterest.getNext() == None):
+        return False # it will return false
+    # otherwise if data associated w/ inputed node does = item
+    elif nodeOfInterest.getData() == item: 
+        return True # function returns true
+    # if data associated w/ inputed node doesn't equal item, and there are nodes after it
+    else: 
+        nodeOfInterest = nodeOfInterest.getNext() # new node = node next to inputed node
+        # it will recursively keep going, if it goes through the entire list, and still hasn't
+        # found match, function will return to the first if statement
+        return findValue(item, nodeOfInterest) 
+                                               
+                                               
+
+# Problem 6: finding last value in a linked list
+
+    # For this problem, I modified the above code so that the function will just go to the
+    # last node and get the data
+def findLastValue(nodeOfInterest): # defining the function 
+    if nodeOfInterest.getNext() == None: # if no node next to the node we put in the parameter
+        return nodeOfInterest.getData() # returns data associated with the nodeOfInterest
+    else: # otherwise 
+        nodeOfInterest = nodeOfInterest.getNext() # new nodeOfInterest becomes the next one
+        # will recursive do this until there is None and then it will return the data
+        # associated with last one
+        return findLastValue(nodeOfInterest) 
+
---
+++ Data Structures/Search_Functions.py
@@ -0,0 +1,140 @@
+# Tannavee Kumar
+
+# Problem 1: create a new function called ternarySearch. Should do what
+# binary search does but now in three segments
+
+def ternarySearch(alist, item):
+    first = 0 # first = first index
+    last = len(alist) - 1 # last = last index
+    found = False
+    while first <= last and not found:
+        midpoint1 = first + (last - first)//3 # length of 1/3 of list and add to 0
+        midpoint2 = last - (last - first)//3 # length of 1/3 of list and sbtrct from last
+        if (alist[midpoint1] == item) or (alist[midpoint2] == item): 
+            found = True # item is in list if either midpoints = items
+        else:
+            if item < alist[midpoint1]: # if item is less than the item at mdpt1
+                last = midpoint1 - 1 # change the last value to the item bfr mdpt1
+            elif item > alist[midpoint2]: # if item is greater than the item at mdpt2
+                first = midpoint2 + 1 # change the 1st value to the item after mdpt2
+            else: # if mdpt1 < item < mdpt2
+                first = midpoint1 + 1 # first item becomes item after mdpt1
+                last = midpoint2 - 1 # last value becomes item after mdpt2
+    return found
+
+a = [1, 5, 7, 9]
+ternarySearch(a, 5)
+
+# Problem 2: creat a new function called ternarySearchRec, which should be the
+# recursive version of the above code.
+
+def ternarySearchRec(alist, item): # defining the recursive function 
+    if alist == []: # base case, if list is empty
+        return False # user will see false
+    else:
+        midpoint1 = len(alist)//3 # mdpt1 at index at 1/3 mark
+        midpoint2 = (len(alist) * 2)//3 # mdpt2 at index at 2/3 mark
+        if (alist[midpoint1] == item) or (alist[midpoint2] == item):
+            return True # true if item = mdpt 1 or mdpt2
+        elif item < alist[midpoint1]: # if item < mdpt 1
+            # base case is the slicing part, recrusily calls from 0, to the value before mdpt1
+            return ternarySearchRec(alist[:midpoint1], item) 
+        elif item > alist[midpoint2]: # if item > mdpt 2
+            # base case is the slicing part, recrusily calls from value after mdpt2
+            # to last value
+            return ternarySearchRec(alist[midpoint2 + 1:], item)
+        else: # if item is in between the mdpts
+            # base case is the slicing part, recrusily calls from value after mdpt1
+            # to value before mdpt2
+            return ternarySearchRec(alist[midpoint1 + 1:midpoint2], item)
+
+# Problem 3: modify the put function from the book, so that the hastable is able
+# be extended 
+
+class HashTable:
+
+    # initializing
+    def __init__(self, size = 11): 
+        # default size is 11, but any size can be initialized
+        self.size = size 
+        # slot list is NONE * size of list, and same for data list
+        self.slots = [None] * self.size 
+        self.data = [None] * self.size
+
+    def put(self,key,data): # put function 
+        hashvalue = self.hashfunction(key,len(self.slots)) # calls the hashfunction 
+        if self.slots[hashvalue] == None: # if slot empty
+            self.slots[hashvalue] = key # that slot holds key
+            self.data[hashvalue] = data # at same index, input data in the data list
+        else: # if the key already exists
+            if self.slots[hashvalue] == key: 
+                self.data[hashvalue] = data  #replace
+            else: # if not he same key, and not empty
+                nextslot = self.rehash(hashvalue,len(self.slots)) # do linear probing
+                # 3 conditions for the while loop to cont probing:
+                # slot can't be empty
+                # slot can't have the same key
+                # nextslot cant = hashvalue, if it does then that means we've done 1 cycle
+                while self.slots[nextslot] != None and self.slots[nextslot] != key and nextslot != hashvalue:
+                    nextslot = self.rehash(nextslot,len(self.slots)) # cont probing
+                
+                if self.slots[nextslot] == None: # if slot is empty
+                    self.slots[nextslot]=key # enter key
+                    self.data[nextslot]=data # enter data in corresponding index
+                
+                elif self.data[nextslot] == key: # if slot has same key
+                    self.data[nextslot] == data # update 
+                
+                else:
+                    if nextslot == hashvalue: # if nextslot = inital hasvalue
+                        self.newSize = (self.size * 2) + 1 # increase size
+                        print("Resizing the table") # let's the user know about resizing
+                        # divide newsize by 3 to newsiz - 1 to check if prime
+                        for i in range(3, self.newSize):
+                            if self.newSize % i == 0: # if remainder = 0
+                                self.newSize = self.newSize + 2 # add 2
+                        print("New Size: ", self.newSize) # print the final newsize
+                        self.oldSlots = self.slots # old slots = the orignal slot array
+                        self.oldData = self.data # old data = original data array
+                        self.slots = [None] * self.newSize # currant slot array is addjusted 
+                        self.data = [None] * self.newSize # currant data array is adjusted
+                        self.oldSize = self.size # old size = original size
+                        self.size = self.newSize # curratn size is updated
+                        for i in range(len(self.oldSlots)): # for the indices in old slot array
+                            self.put(self.oldSlots[i], self.oldData[i]) # put key, data in new arrays
+                        self.put(key, data) # run this for any remained pairs not in the table yet
+                            
+    def hashfunction(self,key,size): # this function provides hasvalue
+        return key%size 
+    
+    def rehash(self,oldhash,size): # used for linear probing 
+        return (oldhash+1)%size
+
+    def get(self,key): # user can input key, and get value 
+        startslot = self.hashfunction(key,len(self.slots)) # gets hashvalue
+        data = None 
+        stop = False
+        found = False
+        position = startslot # counter starts at hashvalue(starting slot)
+        # 3 conditions for while loop for it to stop
+        # slot at postion should be empty, and
+        # nothing found, and
+        # not stopped
+        while self.slots[position] != None and not found and not stop: 
+            if self.slots[position] == key: # if slot has key
+                found = True 
+                data = self.data[position] # value returned is in corresponsing data array
+            else: # otherwise do linear probing to find key
+                position = self.rehash(position,len(self.slots))
+                if position == startslot: # if position returns back to starting slot 
+                    stop = True # loop stops 
+        return data
+
+    def __getitem__(self,key): # using brackets to easily get
+        return self.get(key)
+
+    def __setitem__(self,key,data): # using brackets to easilty put
+        self.put(key,data)
+
+        
+        
